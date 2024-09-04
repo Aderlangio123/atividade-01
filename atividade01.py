b257266dd468927076e83d513d6ee4b117103554
Aderlangio123 <faderlangio123@gmail.com>
2024-09-04 14:40:05 -0300

    atividade 01 - soma de numeros

---
+++ atividade01.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+numero1 = int(input("digite o numero 1"))
+numero2 = int(input("digite o numero 2"))
+
+print("o resultado é ", numero1 + numero2)
